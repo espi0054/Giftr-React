e60de0cc051e9ea151f5e026491a57481fbae2f7
CJ Espiritu <cjespiritu@cjs-macbook-pro.home>
2023-04-16 12:03:03 -0400

    added styling

--- src/components/Gifts.js
+++ src/components/Gifts.js
@@ -1,1 +1,51 @@
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams, Link } from "react-router-dom";
+import { Context } from "../contexts/ContextProvider";
+import config from "../Config/Config";
+import { Card, Container } from "react-bootstrap";
+import { AiOutlineEdit, AiOutlineGift } from "react-icons/ai";
+import NavigationBar from "./Navbar";
 
+
+const Gifts = () => {
+  return (
+    <>
+      <NavigationBar
+        left="back"
+        leftPath="/people"
+        right="add"
+        rightPath={`/people/${userId}/gifts/add`}
+      />
+      {gifts.length === 0 ? (
+        <h1 className="text-white text-center m-2">
+          No Gift Found for {person.name}...
+        </h1>
+      ) : (
+        <Container>
+          <h1 className="text-center text-capitalize my-4 mx-1 text-white">
+            Gift ideas for {person.name}
+          </h1>
+          {gifts.map((gift) => (
+            <Card className="my-3 py-2" key={gift._id}>
+              <Card.Body className="d-flex justify-content-between ">
+                <div>
+                  <Card.Title>{gift.idea}</Card.Title>
+                  <Card.Text> {gift.location}</Card.Text>
+                  <small className="p"> {gift.website}</small>
+                </div>
+                <div className="d-flex align-items-center">
+                  <Link
+                    className="m-1 bg-primary text-white py-2 px-3 h5 rounded me-3"
+                    to={`/people/${userId}/gifts/${gift._id}`}
+                  >
+                    <AiOutlineEdit />
+                  </Link>
+                </div>
+              </Card.Body>
+            </Card>
+          ))}
+        </Container>
+      )}
+    </>
+  );
+};
