49c1893e92904af547a6b8a6d38681ea6ff77c4c
CJ Espiritu <cjespiritu@cjs-macbook-pro.home>
2023-04-16 02:32:59 -0400

    added styling

--- src/components/Gift.js
+++ src/components/Gift.js
@@ -20,4 +20,73 @@ const Gift = () => {
       navigate("/");
     }
   }, [token, navigate]);
-}
+
+  return (
+    <>
+      <NavigationBar left="back" leftPath={`/people/${userId}/gifts`} />
+
+      <Container>
+        <h2 className="text-center text-capitalize my-4 text-white">
+          Edit Gift for {personName}
+        </h2>
+        <Form className="mx-auto">
+          <Form.Group controlId="giftIdea" className="w-75 mx-auto">
+            <Form.Label>Gift idea</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter gift idea"
+              value={giftIdea}
+              onChange={(e) => setGiftIdea(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="storeLocation" className="w-75 mx-auto">
+            <Form.Label>Store | Location</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter store or location"
+              value={storeLocation}
+              onChange={(e) => setStoreLocation(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="websiteURL" className="w-75 mx-auto">
+            <Form.Label>Website URL</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter website URL"
+              value={websiteURL}
+              onChange={(e) => setWebsiteURL(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="w-75 mx-auto mt-2 d-flex justify-content-center align-items-center">
+            <Button
+              variant="primary"
+              type="submit"
+              onClick={handleSave}
+              className="m-2"
+            >
+              Save
+            </Button>
+            <Button
+              variant="danger"
+              className="m-2"
+              type="submit"
+              onClick={(event) => {
+                let del = window.confirm("Are you sure you want delete ? ");
+                console.log(del);
+                if (del) {
+                  handleDelete(event);
+                } else {
+                  event.preventDefault();
+                }
+              }}
+            >
+              Delete
+            </Button>
+          </Form.Group>
+        </Form>
+      </Container>
+    </>
+  );
+};
